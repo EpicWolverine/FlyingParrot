7771da2aa32522d9487f035087af497c7e30153c
EpicWolverine <epicwolverine@me.com>
2018-04-18 22:48:35 -0400

    Fix file upload

--- FlyingParrot/js/controllers/UploadController.js
+++ FlyingParrot/js/controllers/UploadController.js
@@ -1,49 +1,32 @@
 app.controller('UploadController', ['$scope', 'Page', '$http', '$location', function ($scope, Page, $http, $location) {
-	//$http.get('/api/Category/AllSounds')
-	//	.success(function (data) {
-	//		$scope.cats = data;
-	//		//return data;
-	//	})
-	//	.error(function (err) {
-	//		//return err;
-	//	}); 
-	
-	Page.setTitle("Flying Parrot");
+	Page.setTitle("Upload - Flying Parrot");
 
 	$scope.upload = function () {
-		var f = document.getElementById('file').files[0],
-			r = new FileReader();
-
-		r.onloadend = function (e) {
-			var metadata = {
-				"text": $scope.displayText.toString(),
-				"uploader": $scope.uploader,
-				"categoryName": $scope.category,
-				"filename": Date.now().toString()
-			};
-			var data = e.target.result;
-			//send your binary data via $http or $resource or do anything else with it
-			$http({
-				url: "/api/Sound",
-				method: "POST",
-				data: metadata,
-				headers: { 'Content-Type': "application/json" }
-			}).success(function (response) {
-				$http({
-					url: "/api/Sound/Upload?input=" + metadata.filename,
-					method: "POST",
-					data: data,
-					headers: { 'Content-Type': "audio/mpeg" }
-				}).success(function (response) {
-					$location.path("/sound/" + response).replace();
-				}).error(function (response) {
-					alert("Failed to upload file.\n" + response);
-				});
-			}).error(function (response) {
+		var f = document.getElementById('file').files[0];
+		var metadata = {
+			"text": $scope.displayText.toString(),
+			"uploader": $scope.uploader,
+			"categoryName": $scope.category,
+			"filename": Date.now().toString()
+		};
+		$http.post("/api/Sound", metadata, { 'Content-Type': "application/json" })
+			.then(function successCallback(response) {
+				var config = {
+					headers: { "Content-Type": undefined },
+					params: {
+						filename: f.name,
+						size: f.size,
+						type: f.type
+					}
+				};
+				$http.post("/api/Sound/Upload?input=" + metadata.filename, f, config)
+					.then(function successCallback(response) {
+						$location.path("/sound/" + response).replace();
+					}, function errorCallback(response) {
+						alert("Failed to upload file.\n" + response);
+					});
+			}, function errorCallback(response) {
 				alert("Failed to send metadata.\n" + response);
 			});
-		}
-
-		r.readAsBinaryString(f);
-	}
+	};
 }]);
